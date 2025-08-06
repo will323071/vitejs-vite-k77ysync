bf14bb4f05b3506258b9a8f6704953a42bd1cabe
will323071 <cylee323071@gmail.com>
2025-08-06 13:34:10 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import reactLogo from './assets/react.svg';
+import packageJson from '../package.json';
+import './App.css';
+
+//App.jsx를 수정해서는 안됩니다.
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </div>
+      <h1>{packageJson.name}</h1>
+      <div>
+        <div>{`${packageJson.keywords.join(', ')}로 제작`}</div>
+        <p>{packageJson.author} 개발함!</p>
+      </div>
+      <p>프로젝트 버전 {packageJson.version}</p>
+    </>
+  );
+}
+
+export default App;
